67994451d5ea9a01057c01ad6b7a190820f1f8d4
SILVIA GONZALO SANTOS <AL393991@UJI.ES>
2021-03-16 18:39:21 +0100

    Commit inicial

---
+++ R/resta.R
@@ -0,0 +1,15 @@
+#' Funcion resta
+#'
+#' @param a primer parametro
+#' @param b segundo parametro
+#'
+#' @return la resta
+#' @export
+#'
+#' @examples
+#' resta(1,1)
+#'
+#' @seealso \link{suma}
+resta=function(a,b){
+  return(a-b)
+}
---
+++ R/suma.R
@@ -0,0 +1,15 @@
+#' Funcion suma
+#'
+#' @param a primer parametro
+#' @param b segundo parametro
+#'
+#' @return la suma
+#' @export
+#'
+#' @examples
+#' suma(1,1)
+#'
+#' @seealso \link{resta}
+suma=function(a,b){
+  return(a+b)
+}
